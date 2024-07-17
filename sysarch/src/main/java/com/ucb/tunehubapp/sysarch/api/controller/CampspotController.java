1fcad056792258810da1e74cbe6b9391aeb426ee
Jhyzrelfred <jhyzrelfred@gmail.com>
2024-07-18 01:11:44 +0800

    Update CampspotController.java
    Cross origin added

--- sysarch/src/main/java/com/ucb/tunehubapp/sysarch/api/controller/CampspotController.java
+++ sysarch/src/main/java/com/ucb/tunehubapp/sysarch/api/controller/CampspotController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
+@CrossOrigin(origins = "*")
 @RestController
 public class CampspotController {
     @Autowired
